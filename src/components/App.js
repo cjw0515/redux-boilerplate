0d5d1690d60ab61083901437f3ab847453b199e1
DESKTOP-QF4CPAC\Administrator <cjw0515@naver.com>
2018-05-08 22:02:24 +0900

    no message

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import CounterContainer from '../containers/CounterContainer';
+import TodoContainer from '../containers/TodoContainer';
 
 class App extends Component {
   render() {
     return (
       <div>
         <CounterContainer />
+        <TodoContainer />
       </div>
     );
   }
